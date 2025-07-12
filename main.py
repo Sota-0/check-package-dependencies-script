403fbac7966cd8f7c48dd982637c7e7ba6e2314f
Sota <120473819+Sota-0@users.noreply.github.com>
2025-07-12 11:50:20 +1000

    Create main.py
    main script

---
+++ main.py
@@ -0,0 +1,54 @@
+import time
+import os
+import sys
+from colorama import Fore, init
+
+init(autoreset=True)
+
+# change me
+dependencies = ["curl", "nano", "PlaceHolder1", "PlaceHolder2"]
+to_install = []
+a = 0
+
+def main():
+    global a
+    
+    for i in dependencies:
+        check = os.popen(f"apt-cache policy {i}").read()
+        if i not in check:
+            print(Fore.RED + "[-]", i)
+            to_install.append(i)
+            time.sleep(.1)
+            a = 1    
+        else:
+            print(Fore.GREEN + "[+]", i)
+            time.sleep(.1)
+    
+    if a == 1:
+        print("install dependencies? (y / n) : ", to_install)
+
+        while True:
+                command = input(">>")
+
+                if command == "y":
+                    print("installing...")
+                    for i in to_install:
+                        os.system(f"sudo apt-get install {i}")
+                    time.sleep(2)
+
+                elif command == "n":
+                     print(Fore.RED + "Dependencies are REQUIRED")
+                     print("exiting script...")
+                     break
+                
+                else:
+                     print("Please Enter a valid answer: y or n")
+
+    else:
+        print("")
+        print("all requirements are met")
+
+
+
+main()
+
