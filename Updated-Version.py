223c7addc09c3fd9660558a5b652fbb321cd6071
Sota <120473819+Sota-0@users.noreply.github.com>
2026-02-15 15:49:08 +1100

    new updated script
    went to use my script that was uploaded here and realized it was kinda ass. so i made a better looking version that functions properly.
    
    the last one would include already installed packages in the " not installed " section if another came back as not installed.
    
    FIX: added proper package installation handling, effectively separating the installed packages from the not installed packages

---
+++ Updated-Version.py
@@ -0,0 +1,64 @@
+import os
+import time
+from colorama import Fore, init
+
+init(autoreset=True)
+
+
+
+
+#============================
+# Check Dependencies
+#============================
+
+dependencies = ["tmp, tmp, nano, tmp"]
+
+
+# check dependencies
+def check_dependencies():
+
+    to_install = []
+    a = 0
+    
+    for i in dependencies:
+        check = os.popen(f"dpkg -l | awk '$1 == \"ii\" {{print $2}}' | grep -x {i}").read().strip()
+        if i not in check:
+            print(Fore.RED + "[-]", i)
+            to_install.append(i)
+            time.sleep(.1)
+            a = 1
+        else:
+            print(Fore.GREEN + "[+]", i)
+            time.sleep(.1)
+
+    if a == 1:
+        print("install dependencies? (y / n) : ", to_install)
+
+        while True:
+                command = input(">>")
+
+                if command == "y":
+                    print("installing...")
+                    for i in to_install:
+                        os.system(f"sudo apt-get install {i}")
+                    time.sleep(.5)
+                    print("")
+                    print(Fore.MAGENTA + "please re-run the script")
+                    break
+
+                elif command == "n":
+                     print(Fore.RED + "missing dependencies will cause issues")
+                     time.sleep(4)
+                     break
+
+                else:
+                     print("Please Enter a valid answer: y or n")
+
+    else:
+        print("")
+        time.sleep(.5)
+        print("all requirements are met")
+        time.sleep(2)
+        os.system("clear")
+
+    
